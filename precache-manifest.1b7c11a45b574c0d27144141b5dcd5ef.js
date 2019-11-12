f51b218f3125770e5d2b95e4c7b4ca04b1423b57
bbshark99 <blue.shark99@outlook.com>
2019-11-12 11:29:58 +1000

    Updates

---
+++ precache-manifest.1b7c11a45b574c0d27144141b5dcd5ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ee73ef5fcd7845dd4085ef6b8cac4f",
+    "url": "/simple_react_component/index.html"
+  },
+  {
+    "revision": "baea876ed5854faa6424",
+    "url": "/simple_react_component/static/css/main.bab829b1.chunk.css"
+  },
+  {
+    "revision": "e9cacbf2bc110e26e9e8",
+    "url": "/simple_react_component/static/js/2.dfb0cf60.chunk.js"
+  },
+  {
+    "revision": "baea876ed5854faa6424",
+    "url": "/simple_react_component/static/js/main.59349cdb.chunk.js"
+  },
+  {
+    "revision": "ad28094861117bd7baf3",
+    "url": "/simple_react_component/static/js/runtime~main.d8b4f6ed.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/simple_react_component/static/media/logo.ee7cd8ed.svg"
+  }
+]);
